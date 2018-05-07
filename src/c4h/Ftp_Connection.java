54063daee87d8957842d76686aca8ede3626b817
HelmiB2 <34322239+HelmiB2@users.noreply.github.com>
2018-05-08 00:46:22 +0200

    new Class for SFTP

--- src/c4h/Ftp_Connection.java
+++ src/c4h/Ftp_Connection.java
@@ -9,15 +9,14 @@ import org.apache.commons.net.ftp.FTPReply;
 
 
 public class Ftp_Connection {
-	private  final String FTP_HOST           = "";
-	private  final String _USER_NAME         = "";
-	private  final String _USER_PWD          = "";
+	private  final String FTP_HOST           = "3s-hamburg.de";
+	private  final String _USER_NAME         = "GXD2iRx$";
+	private  final String _USER_PWD          = "wswham_2";
 	private        String schulNummer        = "";
 	private  FTPClient ftp                   = null;
 
 	public static void main(String[] args) {
 		Ftp_Connection ftp = new Ftp_Connection("schulnummer");
-		ftp.loginToFtp();
 	}
 	public Ftp_Connection(String schulnummer) {
 		this.schulNummer=schulnummer;
@@ -26,7 +25,7 @@ public class Ftp_Connection {
 	public void loginToFtp() {
 		ftp = new FTPClient();
 		try {
-			ftp.connect(FTP_HOST);
+			ftp.connect(FTP_HOST,21);
 			boolean success = ftp.login(_USER_NAME, _USER_PWD);
             if (!success) {
                 System.out.println("Could not login to the server");
---
+++ src/c4h/SftpConnection.java
@@ -0,0 +1,77 @@
+package src.c4h;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.zip.*;
+import com.jcraft.jsch.*;
+import com.jcraft.jsch.ChannelSftp.LsEntry;
+
+public class SftpConnection {
+	 
+	private  String _FTP_HOST  ;//         = "3s-hamburg.de";
+	private  String _USER_NAME ;//        = "GXD2iRx$";
+	private  String _USER_PWD   ;//       = "wswham_2";
+	private int _PORT;
+	Session session = null;
+	ChannelSftp channel= null;
+	
+	
+	
+	public static void main(String[] args) {
+		SftpConnection clientSftp = null;
+		if( args == null || args.length < 4 ) {
+	         System.out.println( "Fehler: Parameter fehlen." );
+	         System.exit( 1 );
+		}else {
+			clientSftp = new SftpConnection(args[0],args[1],args[2],args[3]);
+			try {
+				clientSftp.uploadFileWithSchoolNumber();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public SftpConnection( String benutzername, String passwort, String host, String port ){
+		this._FTP_HOST=host;
+		this._USER_NAME= benutzername;
+		this._USER_PWD=passwort;
+		this._PORT= Integer.parseInt(port);
+		 /*
+		  *  Die erste Option (StrictHostKeyChecking) schaltet die Rückfragen aus, 
+		  *  die eine SSH-Installation üblicherweise dann stellt,
+		  *   wenn Sie sich das erste Mal mit dem jeweiligen Host verbinden
+		  */
+		
+		try {
+			session = (new JSch()).getSession( _USER_NAME,_FTP_HOST , _PORT );
+			session.setPassword( passwort );
+			session.setConfig( "StrictHostKeyChecking", "no" );
+	        session.connect();
+		} catch (JSchException e) {
+			System.out.println(e);
+		}
+		 
+		 try {
+			channel = (ChannelSftp) session.openChannel( "sftp" );
+			channel.connect();
+		} catch (JSchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public  void uploadFileWithSchoolNumber() throws Exception {
+		
+		try {
+	         channel.put( "localSrcFilePath", "remoteDstFilePath" );
+	      } catch( SftpException ex ) {
+	         throw new IOException( ex );
+	      }
+		
+	}
+}
