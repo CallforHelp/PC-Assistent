4c5c4579d9f764707dff983e7f2d10f5c8fb1dde
HelmiB2 <34322239+HelmiB2@users.noreply.github.com>
2018-06-20 15:15:36 +0200

    addoumouseClickListner for C4H Icon Tray

--- src/c4h/C4H_GUI_TRAY_ICON.java
+++ src/c4h/C4H_GUI_TRAY_ICON.java
@@ -9,6 +9,8 @@ import java.awt.TrayIcon;
 import java.awt.TrayIcon.MessageType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
@@ -28,62 +30,69 @@ public class C4H_GUI_TRAY_ICON {
  * Ein Menu F&uuml;hrung zur bedienung ist auch vorhanden(pc-information, support,chat)
  * @throws Throwable IconTray
  */
-public void createshowGui() throws Throwable {
-    C4H_DYNAMIC_GUI window =  new C4H_DYNAMIC_GUI();
-    window.initialize();
-		final C4H_PC_INFO_KLASSE bg = new C4H_PC_INFO_KLASSE();
-		
-		
-		if (!SystemTray.isSupported()) {
-			System.out.println("SystemTray is not supported");
-			return;
-		}
-		
+		public void createshowGui() throws Throwable {
+	
+			C4H_DYNAMIC_GUI window =  new C4H_DYNAMIC_GUI();
+			window.initialize();
+			final C4H_PC_INFO_KLASSE bg = new C4H_PC_INFO_KLASSE();
+	
+			if (!SystemTray.isSupported()) {
+				System.out.println("SystemTray is not supported");
+				return;
+			}
 		final PopupMenu popup = new PopupMenu();
 		final TrayIcon trayIcon =
-                new TrayIcon(createImage("images/bulb.png", "trayIcon"));  
-        final SystemTray tray = SystemTray.getSystemTray();
-        
-        //Hinzufuegen(start) the Icon_tray 
-        try {
-            tray.add(trayIcon);
-            System.out.println("Schulnummer Aktuell: "+bg.getSchulNummer()+" ist :"+ bg.pruefeSchulnr());
-            if(!bg.pruefeSchulnr())
+			new TrayIcon(createImage("images/bulb.png", "trayIcon"));  
+		final SystemTray tray = SystemTray.getSystemTray();
+		//Hinzufuegen(start) the Icon_tray 
+		try {
+			tray.add(trayIcon);
+			System.out.println("Schulnummer Aktuell: "+bg.getSchulNummer()+" ist :"+ bg.pruefeSchulnr());
+			if(!bg.pruefeSchulnr())
 				trayIcon.displayMessage("Die SchulNummer ist Fehlerhaft!!!"+bg.getSchulNummer(),
 						"Schul Support Service\nTEAM C4H", MessageType.WARNING);
-    	}catch(Throwable e2) {
-                System.out.println("TrayIcon could not be added."+e2.getMessage());
-                return;
-    	}
+    			}catch(Throwable e2) {
+    				System.out.println("TrayIcon could not be added."+e2.getMessage());
+    			return;
+    		}
             
-        MenuItem BG_Info_MenuItem = new MenuItem("PC Information");
-        MenuItem Fehler_Meldung_Menu_Item = new MenuItem("Support");
-        MenuItem Chat_Menu_Item = new MenuItem("Chat");
+    	MenuItem BG_Info_MenuItem = new MenuItem("PC Information");
+    	MenuItem Fehler_Meldung_Menu_Item = new MenuItem("Support");
+    	MenuItem Chat_Menu_Item = new MenuItem("Chat");
         
 
        
 
-      MenuItem Beenden_Menu_Item = new MenuItem("Beenden");
-        popup.add(Beenden_Menu_Item);
-        Beenden_Menu_Item.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// todo hier
-				System.exit(0);
-				
-			}
+    	MenuItem Beenden_Menu_Item = new MenuItem("Beenden");
+    	popup.add(Beenden_Menu_Item);
+    	Beenden_Menu_Item.addActionListener(new ActionListener() {
+    		@Override
+    		public void actionPerformed(ActionEvent e) {
+    			System.exit(0);
+    		}
 		});
-        popup.add(BG_Info_MenuItem);
-        popup.add(Fehler_Meldung_Menu_Item);
-        popup.add(Chat_Menu_Item);
+    	popup.add(BG_Info_MenuItem);
+    	popup.add(Fehler_Meldung_Menu_Item);
+    	popup.add(Chat_Menu_Item);
         
         
         //MenuListe Einsetzen
-        trayIcon.setPopupMenu(popup);
+    	trayIcon.setPopupMenu(popup);
         
-   
-        //action
-        trayIcon.addActionListener(new ActionListener() {
+    	//action
+    	trayIcon.addMouseListener(new MouseAdapter() {
+    		public void mouseClicked(MouseEvent e ) {
+    			if(e.getButton()==MouseEvent.BUTTON1) {
+    				window.frmCh.setVisible(true);
+    				window.PcInfoPanel.setVisible(true);
+    				window.FehlerMeldenPanel.setVisible(false);
+    				window.chatPanel.setVisible(false);
+    			}
+			
+    		}
+    	});
+       
+    	trayIcon.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("action by click");
@@ -95,14 +104,13 @@ public void createshowGui() throws Throwable {
 						dialog.dispose();
 					}
 				});
-			timer.setRepeats(false);
-			timer.start();
-			}
-			
+				timer.setRepeats(false);
+				timer.start();
+			}	
 		});
         
-        BG_Info_MenuItem.addActionListener(new ActionListener() {
-			@Override
+    	BG_Info_MenuItem.addActionListener(new ActionListener() {
+    		@Override
 			public void actionPerformed(ActionEvent e) {
 				//todo hier
 				try {
@@ -111,16 +119,13 @@ public void createshowGui() throws Throwable {
 					window.PcInfoPanel.setVisible(true);
 					window.FehlerMeldenPanel.setVisible(false);
 					window.chatPanel.setVisible(false);
-					//window.tabbedPane.setSelectedIndex(window.tabbedPane.getTabCount()-3);
 				} catch (Throwable e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
-				}
-				
+				}	
 			}
 		});
         
-        Fehler_Meldung_Menu_Item.addActionListener(new ActionListener() {
+    	Fehler_Meldung_Menu_Item.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				window.frmCh.setVisible(true);
@@ -131,15 +136,15 @@ public void createshowGui() throws Throwable {
 			}
 		});
         
-        		Chat_Menu_Item.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				window.frmCh.setVisible(true);
-				window.chatPanel.setVisible(true);
-				window.PcInfoPanel.setVisible(false);
-				window.FehlerMeldenPanel.setVisible(false);
-			}
-		});
+    	Chat_Menu_Item.addActionListener(new ActionListener() {
+    		@Override
+    		public void actionPerformed(ActionEvent e) {
+    			window.frmCh.setVisible(true);
+    			window.chatPanel.setVisible(true);
+    			window.PcInfoPanel.setVisible(false);
+    			window.FehlerMeldenPanel.setVisible(false);
+    		}
+    	});
 	}
 	/**
 	 * F&uuml;r die realisierung einens Icon f&uuml;r C4H
@@ -148,13 +153,13 @@ public void createshowGui() throws Throwable {
 	 * @return pic
 	 * @wbp.parser.entryPoint
 	 */
-	protected static Image createImage(String path, String description) {
-    	URL imageURL = C4H_GUI_TRAY_ICON.class.getResource(path);
-    	if (imageURL == null) {
-            System.err.println("Resource not found: " + path);
-            return null;
-        } else {
-            return (new ImageIcon(imageURL, description)).getImage();
-        }
-    }
+		protected static Image createImage(String path, String description) {
+			URL imageURL = C4H_GUI_TRAY_ICON.class.getResource(path);
+			if (imageURL == null) {
+				System.err.println("Resource not found: " + path);
+				return null;
+			} else {
+				return (new ImageIcon(imageURL, description)).getImage();
+			}
+		}
 }
