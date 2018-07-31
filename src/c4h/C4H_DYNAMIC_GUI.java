db5935dd9765e2c57de60700a6398032419d4085
HelmiB2 <34322239+HelmiB2@users.noreply.github.com>
2018-07-31 11:18:58 +0200

    import organize

--- src/c4h/C4H_DYNAMIC_GUI.java
+++ src/c4h/C4H_DYNAMIC_GUI.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dialog.ModalExclusionType;
-import java.awt.TrayIcon.MessageType;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
