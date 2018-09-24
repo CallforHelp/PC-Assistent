0f537aa706eadee7de02c0f6b805cd6e1df39379
HelmiB2 <34322239+HelmiB2@users.noreply.github.com>
2018-09-24 11:47:59 +0200

    new Class Logfile

---
+++ src/c4h/C4H_LOG_FILE.java
@@ -0,0 +1,43 @@
+package src.c4h;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class C4H_LOG_FILE {
+  
+  FileWriter writer;
+  File file;
+  
+  public void schreiben(String loginfotext){
+    // File anlegen
+     file = new File("d:\\FileWriterTest.txt");
+     try {
+       // new FileWriter(file ,true) - falls die Datei bereits existiert
+       // werden die Bytes an das Ende der Datei geschrieben
+       
+       // new FileWriter(file) - falls die Datei bereits existiert
+       // wird diese überschrieben
+       writer = new FileWriter(file ,true);
+       
+       // Text wird in den Stream geschrieben
+       writer.write(loginfotext);
+       
+       // Platformunabhängiger Zeilenumbruch wird in den Stream geschrieben
+       writer.write(System.getProperty("line.separator"));
+       
+       // Schreibt den Stream in die Datei
+       // Sollte immer am Ende ausgeführt werden, sodass der Stream 
+       // leer ist und alles in der Datei steht.
+       writer.flush();
+       
+       // Schließt den Stream
+       writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
